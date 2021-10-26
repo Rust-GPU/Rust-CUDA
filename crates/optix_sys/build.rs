6c92c696d2579256bb05ce790ac0e5b51104e321
Anders Langlands <anderslanglands@gmail.com>
2021-10-26 21:27:55 +1300

    Get build working on unix
    Had to add cuda's include directory to cc

--- crates/optix_sys/build.rs
+++ crates/optix_sys/build.rs
@@ -1,4 +1,4 @@
-use find_cuda_helper::find_optix_root;
+use find_cuda_helper::{find_cuda_root, find_optix_root};
 use std::env;
 
 // OptiX is a bit exotic in how it provides its functions. It uses a function table
@@ -8,15 +8,22 @@ use std::env;
 // to a lib and link it in so that we have the initialization and C function logic.
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
-    let mut header = find_optix_root().expect(
+    let mut optix_include = find_optix_root().expect(
         "Unable to find the OptiX SDK, make sure you installed it and
     that OPTIX_ROOT or OPTIX_ROOT_DIR are set",
     );
-    header = header.join("include");
+    optix_include = optix_include.join("include");
+
+    let mut cuda_include = find_cuda_root().expect(
+        "Unable to find the CUDA Toolkit, make sure you installed it and
+    that CUDA_ROOT, CUDA_PATH or CUDA_TOOLKIT_ROOT_DIR are set",
+    );
+    cuda_include = cuda_include.join("include");
 
     cc::Build::new()
         .file("./optix_stubs.c")
-        .include(header)
+        .include(optix_include)
+        .include(cuda_include)
         .cpp(false)
         .compile("optix_stubs");
 
