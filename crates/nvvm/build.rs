f1f29cb3323f18a781fb96fd11f15073d978052f
Anders Langlands <anderslanglands@gmail.com>
2021-11-08 13:48:04 +1300

    Use find_cuda_helper to get cuda path

--- crates/nvvm/build.rs
+++ crates/nvvm/build.rs
@@ -13,7 +13,16 @@ fn libnvvm_build() {
     // on windows, libnvvm should be in CUDA_PATH/nvvm/
     // println!("cargo:rustc-link-lib=dylib=../../../Program Files/NVIDIA GPU
     // Computing Toolkit/CUDA/v11.3/nvvm/bin/nvvm64_40_0");
-    println!("cargo:rustc-link-search={}", lib_search_path());
+
+    let cuda_root = find_cuda_helper::find_cuda_root().expect("Could not figure out cuda installation root");
+
+    #[cfg(windows)]
+    let lib_search_path = std::path::Path::new(&cuda_root).join("nvvm").join("lib").join("x64");
+
+    #[cfg(unix)]
+    let lib_search_path = std::path::Path::new(&cuda_root).join("nvvm").join("lib64");
+
+    println!("cargo:rustc-link-search={}", lib_search_path.display());
     println!("cargo:rustc-link-lib=dylib=nvvm");
 }
 
