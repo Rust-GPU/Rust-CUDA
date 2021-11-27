6a4d31a9912f45badfddbd5ad3de770ce7c8fd2c
rdambrosio <rdambrosio016@gmail.com>
2021-11-27 02:23:23 -0500

    Feat: rename path_tracer kernels name and commit ptx files

--- examples/cuda/cpu/path_tracer/build.rs
+++ examples/cuda/cpu/path_tracer/build.rs
@@ -2,7 +2,7 @@ use cuda_builder::CudaBuilder;
 
 fn main() {
     CudaBuilder::new("../../gpu/path_tracer_gpu")
-        .copy_to("../../resources/kernels.ptx")
+        .copy_to("../../resources/path_tracer.ptx")
         .build()
         .unwrap();
 }
