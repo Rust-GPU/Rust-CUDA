32c92974b97cec028e8ba6636d18a957ecd819e0
Christian Legnitto <christian@legnitto.com>
2025-04-01 19:37:58 -0400

    Make tool work

---
+++ .ci/cargo_glue.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# This script is used by cargo to run the Modal test runner with the 
+# specified arguments.
+#
+# We need this script because the cwd that cargo runs the runner in changes
+# depending on crate.
+
+cd "$GITHUB_WORKSPACE/.ci/modal.com" && uv run modal_test_runner.py "$@"
