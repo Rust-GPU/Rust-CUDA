e343e6aa57b1c7f6dfaa13cb992f02cc3b3b9890
Christian Legnitto <christian@legnitto.com>
2025-04-15 17:38:04 -0400

    Add a custom no-op cargo test runner

---
+++ .ci/cargo_glue.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# This script is used by cargo to run the test runner with the 
+# specified arguments.
+#
+# We need this script because the cwd that cargo runs the runner in changes
+# depending on crate.
+
+cd "$GITHUB_WORKSPACE/.ci/;
+echo "In glue script with cwd: $(pwd)"
