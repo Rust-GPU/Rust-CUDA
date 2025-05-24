3b3e0497227c913cadf54310f18e4eda8b10f5e3
Christian Legnitto <LegNeato@users.noreply.github.com>
2025-05-24 09:05:20 -0400

    Use `MANIFEST_DIR` in build scripts (#215)
    This should be a no-op unless running cargo from
    a different location using `--manifest-path`.

--- examples/cuda/vecadd/build.rs
+++ examples/cuda/vecadd/build.rs
@@ -4,11 +4,13 @@ use std::path;
 use cuda_builder::CudaBuilder;
 
 fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=kernels");
+    println!("cargo::rerun-if-changed=build.rs");
+    println!("cargo::rerun-if-changed=kernels");
 
     let out_path = path::PathBuf::from(env::var("OUT_DIR").unwrap());
-    CudaBuilder::new("kernels")
+    let manifest_dir = path::PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+
+    CudaBuilder::new(manifest_dir.join("kernels"))
         .copy_to(out_path.join("kernels.ptx"))
         .build()
         .unwrap();
